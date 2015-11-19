e337e078c5c10f2e4f63bd88677fb8f3cd898084
Mathieu Rolfo <rolfom01@gmail.com>
2015-11-18 17:44:42 -0800

    Added reading in a class list from a file

---
+++ README.txt~
@@ -0,0 +1,1 @@
+This is the repository for an automatic program scheduler for Symbolic Systems, to tell you which classes you've required and how many more classes you need to take for each of the tracks.
--- curriculum.py
+++ curriculum.py
@@ -1,6 +1,6 @@
 # imports
 
-
+import sys
 
 
 class Curriculum:
@@ -11,17 +11,18 @@ class Curriculum:
 	def __init__(self):
 		self.core = [[] for x in xrange(self.numCoreReqs)]
 		self.concentration = [] #dummy for now
+		self.classList = []
 
-	def add_official_core(self):
+	def add_official_core(self): # still a basic version for testing
 
 		self.core[0].append("symsys100")
 		
-		#skipping complex math requirements here
 		self.core[1].append("math51")
 		self.core[1].append("cme100")
 		self.core[1].append("cme100a")
 		self.core[1].append("math51a")
 		self.core[1].append("math51h")
+
 
 	def printCurriculum(self):
 		for i in range(self.numCoreReqs):
@@ -29,13 +30,21 @@ class Curriculum:
 			print str(self.core[i]).strip('[]'),
 			print "}"
 
-	def add_to_core(self, class, category):3
+	#def add_to_core(self, class, category):
+		# complex logic will go here
 
-
+	def add_class_list(self, filename):
+		file = open(filename, 'r')
+		classList = file.readlines()
+		for i in range(len(classList)):
+			classList[i] = classList[i][:-1]
+		print classList
+		self.classList = classList
 
 def create_curriculum():
 	curriculum = Curriculum()
 	curriculum.add_official_core()
 	curriculum.printCurriculum()
+	curriculum.add_class_list(sys.argv[1])
 
 create_curriculum();
